beea89d8a15267e08bebcf14b6d560cda36c6454
Victor Milovanov <lost@losttech.software>
2018-08-09 18:25:20 -0700

    enabled automatic versioning

---
+++ src/WPF/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyCompany("Lost Tech LLC")]
+[assembly: AssemblyCopyright("Copyright © Lost Tech LLC 2018")]
+
+[assembly: ComVisible(false)]
