3b820dbd262f43c76125e81c8efd753eed2973de
Victor Milovanov <lost@losttech.software>
2021-02-27 13:03:21 -0800

    updated copyright year

--- src/WPF/AssemblyInfo.cs
+++ src/WPF/AssemblyInfo.cs
@@ -3,6 +3,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyCompany("Lost Tech LLC")]
-[assembly: AssemblyCopyright("Copyright © Lost Tech LLC 2019")]
+[assembly: AssemblyCopyright("Copyright © Lost Tech LLC 2021")]
 
 [assembly: ComVisible(false)]
